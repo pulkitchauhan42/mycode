1f9f03753acfeed47b6a1994786686c2ff52530c
pulkitchauhan42 <pulkitchauhan42@users.noreply.github.com>
2022-08-23 20:10:52 +0000

    working with methods

---
+++ methodschallenge2.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+        "fmt"
+
+    )
+
+type Virtmach struct{
+
+    ip string
+    hostname string
+    diskgb int
+    ram int
+
+}
+
+func (v *Virtmach)ipset(ip string){
+    v.ip = ip
+}
+
+func (v *Virtmach)diskexpand(gb int){
+    v.diskgb = v.diskgb + gb
+}
+
+func (v Virtmach)display(){
+
+    fmt.Println("Primary IP Address:", v.ip)
+    fmt.Println("Hostname:", v.hostname)        // hostname
+    fmt.Println("Disk GB:", v.diskgb)           // diskgb
+    fmt.Println("RAM:", v.ram)
+}
+
+func main() {
+
+    vm1 := Virtmach{"10.0.0.5", "zebra", 20, 8}  // ip, hostname, diskgb, ram
+    vm1.display()                   // show the current state
+    vm1.diskexpand(3)               // increase by 3 GB
+    vm1.ipset("192.168.77.33")      // change the IP address
+    vm1.display()                   // show the changes
+}
+
